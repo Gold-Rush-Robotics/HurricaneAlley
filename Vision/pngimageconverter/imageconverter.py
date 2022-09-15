214da0309f6e5b16f44c0019d4ebe6460676d329
lucastherrien <ltherrie@uncc.edu>
2022-09-15 18:38:00 -0400

    Image converter now removes old files

--- Vision/pngimageconverter/imageconverter.py
+++ Vision/pngimageconverter/imageconverter.py
@@ -1,12 +1,8 @@
-# Make sure you have Pillow library installed
-# Windows: py -m pip install Pillow
-# Linux: pip install Pillow
-
 import os
 from PIL import Image
 
 # Change the below to your directory containing your images
-image_dir = 'your_path_here'
+image_dir = 'C:/Users/lucas/Documents/CAR_2023_Vision/images'
 
 # Janky converter code
 os.chdir(image_dir)
@@ -14,5 +10,6 @@ for file in os.listdir(image_dir):
     img = Image.open(file)
     file_name, file_ext = os.path.splitext(file)
     img.save(file_name+'.png')
+    os.remove(file_name+file_ext)
 
 

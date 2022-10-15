aff5e12d4a9c50ac85c7b09eea2cf908ec5ebd44
Philip Smith <philip@randomsmiths.com>
2022-10-15 11:05:19 -0400

    added robot

---
+++ Code/Robot/robot.h
@@ -0,0 +1,9 @@
+#include "drivetrain.h"
+
+class Robot{
+    public:
+        Robot();
+        driveController(double s1y, double s1x, double s2y, double s2x);
+    private:
+        Drivetrain drivetrain;
+};

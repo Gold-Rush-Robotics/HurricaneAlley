78af8b015c6b0825ca9bfd742c17c92e30aee3ed
Philip Smith <philip@randomsmiths.com>
2022-10-15 11:05:12 -0400

    added drivetrain

--- Code/Robot/drivetrain.h
+++ Code/Robot/drivetrain.h
@@ -1,6 +1,20 @@
+#include "motor.h"
+#include "encoder.h"
+
+#include "bcm2835.h"
+#include "pca9685.h"
+
+
 class Drivetrain{
     public:
+        Drivetrain(PCA9685 pca9685);
+        void drivePow(double xPow, double yPow, double headPow);
+        void drive(double dx, double dy, double dTheta);
+
 
     private:
-        
-}
+        Motor motors[4];
+        Encoder encoders[3];
+        PCA9685 pca9685;
+        void driveM(double fl, double fr, double bl, double br);
+};

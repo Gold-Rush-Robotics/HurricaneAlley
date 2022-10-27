bcebc1281084aa314eb9a5abaf10ed418fd4e388
Raspberry Pi 4 <batterchox@gmail.com>
2022-10-27 19:52:24 -0400

    drivetrain pointer

--- Code/Robot/robot.cpp
+++ Code/Robot/robot.cpp
@@ -23,20 +23,18 @@ Robot::Robot(){
 
 void Robot::start(){
     // Set up pwm hat connection
-    std::cout << "spot 1a" << std::endl;
     if (bcm2835_init() != 1)
     {
         fprintf(stderr, "bcm2835_init() failed\n");
     }
-    std::cout << "spot 2a" << std::endl;
     PCA9685 pca9685;
     pca9685.Dump();
     pca9685.SetFrequency(1000);
-
-    drivetrain = Drivetrain(pca9685);
+    drivetrain->init(pca9685);
 }
 
 void Robot::driveController(double s1y, double s1x, double s2y, double s2x, double multiplier){
+    std::cout << "Ly:" << s1y << " |Lx:" << s1x << " |Ry:" << s2y << " |Rx:" << s2x << std::endl;
     drivetrain->drivePow(multiplier * s1y, multiplier * s1x, multiplier * -s2x);
 }
 
--- Code/Robot/robot.h
+++ Code/Robot/robot.h
@@ -3,6 +3,7 @@
 
 class Robot{
     public:
+        PCA9685 pca9685;
         void start();
         void driveController(double s1y, double s1x, double s2y, double s2x, double multiplier);
         void stop();

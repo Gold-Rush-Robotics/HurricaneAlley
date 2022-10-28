3be5d48bdadcd9fa1600c450f9079bf39a67a327
Raspberry Pi 4 <batterchox@gmail.com>
2022-10-27 20:54:42 -0400

    FUCK

--- Code/Robot/robot.h
+++ Code/Robot/robot.h
@@ -1,9 +1,26 @@
-#include "drivetrain.h"
 #include <iostream>
+#include "HardwareInterfaces/hardware.h"
+#include "bcm2835.h"
+#include "pca9685.h"
+
+class Drivetrain{
+    public:
+        Motor fl;
+        Motor fr;
+        Motor bl;
+        Motor br;
+        PCA9685* pca9685;
+        void init();
+        void drivePow(double forward, double strafe, double turn);
+        void drive(double dx, double dy, double dTheta);
+        void stop();
+    private:
+        void driveM(double fl, double fr, double bl, double br);
+};
 
 class Robot{
     public:
-        PCA9685 pca9685;
+        static PCA9685* pca9685;
         void start();
         void driveController(double s1y, double s1x, double s2y, double s2x, double multiplier);
         void stop();

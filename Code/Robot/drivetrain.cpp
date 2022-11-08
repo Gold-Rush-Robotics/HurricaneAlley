1aab1a0349debf4ce4a9daa07544ec41bb2242ca
Philip Smith <philip@randomsmiths.com>
2022-11-07 20:34:08 -0500

    move all the drivetrain code (and update it)

---
+++ Code/Robot/drivetrain.cpp
@@ -0,0 +1,60 @@
+#include "drivetrain.h"
+#include <memory>
+
+Drivetrain::Drivetrain(std::shared_ptr<PCA9685> pca){
+    pca9685 = pca;
+    fl = std::make_shared<Motor>(4, 0, pca);
+    fr = std::make_shared<Motor>(17, 1, pca);
+    bl = std::make_shared<Motor>(22, 3, pca);
+    br = std::make_shared<Motor>(27, 2, pca);
+    fl->reverse();
+    bl->reverse();
+    br->reverse();
+}
+
+
+void Drivetrain::drivePow(double forward, double strafe, double turn){
+    std::cout << forward << "|" << strafe << "|" << turn << std::endl;
+    double powerFL = forward + strafe - turn;
+    double powerFR = forward - strafe + turn;
+    double powerBL = forward - strafe - turn;
+    double powerBR = forward + strafe + turn;
+    std::cout << powerFL << "|" << powerFR << "|" << powerBL << "|" << powerBR << std::endl;
+    double powerMax = std::max(
+        abs(powerFL), 
+        std::max(
+            abs(powerFR), 
+            std::max(
+                abs(powerBL), 
+                abs(powerBR))));
+    std::cout << powerMax << std::endl;
+
+    if (powerMax > 1.0)
+    {
+        powerFL /= powerMax;
+        powerFR /= powerMax;
+        powerBL /= powerMax;
+        powerBR /= powerMax;
+    }
+
+    powerFL = constrain(-1.0, 1.0, powerFL);
+    powerFR = constrain(-1.0, 1.0, powerFR);
+    powerBL = constrain(-1.0, 1.0, powerBL);
+    powerBR = constrain(-1.0, 1.0, powerBR);
+
+    printf("\n\n\nfl: %.2f fr: %.2f\n\nbl: %.2f br: %.2f\n", powerFL, powerFR, powerBL, powerBR);
+
+    driveM(powerFL, powerFR, powerBL, powerBR);
+}
+
+
+void Drivetrain::driveM(double fl, double fr, double bl, double br){
+    Drivetrain::fl->setPower(fl);
+    Drivetrain::fr->setPower(fr);
+    Drivetrain::bl->setPower(bl);
+    Drivetrain::br->setPower(br);
+}
+
+void Drivetrain::stop(){
+    driveM(0,0,0,0);
+}

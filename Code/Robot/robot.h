a8f1eaa3ea282f7d4ce39b36285f54d60df8f096
Philip Smith <philip@randomsmiths.com>
2022-11-07 20:16:29 -0500

    pull drivetrain out of robot file

--- Code/Robot/robot.h
+++ Code/Robot/robot.h
@@ -1,29 +1,16 @@
 #include <iostream>
-#include "HardwareInterfaces/hardware.h"
+#include "../HardwareInterfaces/hardware.h"
 #include "bcm2835.h"
 #include "pca9685.h"
-
-class Drivetrain{
-    public:
-        Motor fl;
-        Motor fr;
-        Motor bl;
-        Motor br;
-        PCA9685* pca9685;
-        void init(PCA9685* pca);
-        void drivePow(double forward, double strafe, double turn);
-        void drive(double dx, double dy, double dTheta);
-        void stop();
-    private:
-        void driveM(double fl, double fr, double bl, double br);
-};
+#include "drivetrain.h"
 
 class Robot{
     public:
-        static PCA9685* pca9685;
+        Robot();
+        std::shared_ptr<PCA9685> pca9685;
         void start();
         void driveController(double s1y, double s1x, double s2y, double s2x, double multiplier);
         void stop();
     private:
-        Drivetrain* drivetrain;
+        Drivetrain drivetrain;
 };

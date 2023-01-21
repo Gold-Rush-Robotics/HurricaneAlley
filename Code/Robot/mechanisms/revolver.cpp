acd22cbfa01602263b684759b474ce71795d24be
samofuture <samovarfuture@gmail.com>
2023-01-20 23:23:18 -0500

    Fleshed out some methods, need to get more information

--- Code/Robot/mechanisms/revolver.cpp
+++ Code/Robot/mechanisms/revolver.cpp
@@ -1,41 +1,67 @@
 #include "revolver.h"
 #include <memory>
 
-Revolver::Revolver(std::shared_ptr<PCA9685> pca){
+Revolver::Revolver(std::shared_ptr<PCA9685> pca, std::shared_ptr<EncoderHandler> h)
+{
     pca9685 = pca;
+    enc = h;
+    
+    // Initializing Encoder stuff here until phillip says it's wrong
+    enc->init();
+
     //TODO: Update Pins
-    revolver = std::make_shared<Motor>(17, 0, pca);
+    motor_revolver = std::make_shared<Motor>(17, 0, pca);
     agitator = std::make_shared<Motor>(17, 0, pca);
 
     loader = std::make_shared<Servo>(17, pca);
     opener = std::make_shared<Servo>(17, pca);
     dropper = std::make_shared<Servo>(17, pca);
+
+    //Initializing Revolver to Empty
+    revolver = [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY];
+    //TODO Find encoder ticks for each position
+    chambers = [0, 1, 2, 3, 4];
 }
 
-Revolver::turn_on_agitator(){
+// Look at the name
+Revolver::turn_on_agitator()
+{
     Revolver::agitator->setPower(1.0);
 }
 
-Revolver::turn_off_agitator(){
+// Look at the name
+Revolver::turn_off_agitator()
+{
     Revolver::agitator->setPower(0.0);
 }
 
-Revolver::rotate_revolver(){
+// Rotates the revolver to one of the chambers (int pos)
+Revolver::rotate_revolver(int pos)
+{
+    //TODO Find the encoder number for revolver
+    encoder_ticks = enc->getPos(3);
+
+    //TODO Find Suitable Power to run the motor at and which direction it should turn
+    double power = (encoder_ticks - chambers[pos] < 0) ? 1.0 : -1.0;
+
+    //TODO add some kind of range that it can be between
+    while (enc->getPos(3) != chambers[pos])
+    {
+        motor_revolver->setPower(power);
+    }
+}
+
+Revolver::release_can()
+{
 
 }
 
-Revolver::release_can(){
+Revolver::store_can()
+{
 
 }
 
-Revolver::store_can(){
-
-}
-
-Revolver::rotate_revolver(){
-
-}
-
-Revolver::load_marshmallow(){
+Revolver::load_marshmallow()
+{
 
 }
--- Code/Robot/mechanisms/revolver.h
+++ Code/Robot/mechanisms/revolver.h
@@ -7,7 +7,7 @@
 
 class Revolver{
     public:
-        Revolver(std::shared_ptr<PCA9685> pca, std::shared_ptr<EncoderHandler>);
+        Revolver(std::shared_ptr<PCA9685> pca, std::shared_ptr<EncoderHandler> h);
         
         int turn_on_agitator();
         int turn_off_agitator();
@@ -17,14 +17,25 @@ class Revolver{
 
         int load_marshmallow(MARSHMALLOWS color);
 
-        enum MARSHMALLOWS { RED, GREEN, WHITE };
+        enum MARSHMALLOWS { 
+            EMPTY,
+            RED, 
+            GREEN, 
+            WHITE
+        };
     
     private:
+        std::shared_ptr<PCA9685> pca9685;
+        std::shared_ptr<EncoderHandler> enc;
         std::shared_ptr<Servo> loader, opener, dropper;
         std::shared_ptr<Motor> agitator, motor_revolver;
+        
         MARSHMALLOWS revolver[5], can[3];
-
+        int32_t chambers[5];
+        
         int rotate_revolver(int pos);
+
+        int32_t encoder_ticks;
 
 
 };

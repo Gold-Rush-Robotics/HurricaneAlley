623cbff110b7a1bbfc8f57860a637ccce873e725
Philip Smith <philip@randomsmiths.com>
2023-01-20 19:46:34 -0500

    adding initLoop

--- Code/Opmodes/Runner.cpp
+++ Code/Opmodes/Runner.cpp
@@ -1,11 +1,21 @@
 #include "Runner.h"
 
+/* This function initializes all the needed values
+Base Behavior: initialize robot and advance to the initLoop*/
 int Runner::init(){
     //Initialize all the thingies
     Runner::robot.start();
-    
+    Runner::initLoop();
+    return 0;
 }
 
+/* This function will run after init until told to go to loop (light turning on)
+Base behavior: Imediatly advance*/
+int Runner::initLoop(){
+    return Runner::loop();
+}
+/* Loops Forever, and handles the processes
+Base behavior: Imediately die*/
 int Runner::loop(){
-
+    return 0;
 }
--- Code/Opmodes/Runner.h
+++ Code/Opmodes/Runner.h
@@ -1,12 +1,14 @@
 #include "../Robot/robot.h"
 #include "../Actions/GoldRushAction.h"
 
+/*base runner class to be extended by teleop and auto*/
 class Runner{
     public:
         int seconds = 0;
         Robot robot;
         virtual int init();
         virtual int loop();
+        virtual int initLoop();
     private:
         GoldRushAction currentAction;
 };

6a2982a3309ae79cbce73a3e96ddfa954e1b6ae5
Philip Smith <philip@randomsmiths.com>
2023-01-20 20:39:25 -0500

    make the state machine

--- Code/Opmodes/Auto.cpp
+++ Code/Opmodes/Auto.cpp
@@ -4,14 +4,20 @@ class Auto : Runner
 {
     int init() override
     {
-
+        Runner::init();
     }
     int loop() override
     {
-
+        if(currentAction != nullptr){
+            currentAction = currentAction.run();
+        }
     }
     int initLoop() override {
-
+        bool light = true;
+        while(!light){
+            
+        }
+        loop();
     }
 };
 

1205141358d6fea772a5acab6015847316aa655f
philip <philip@randomsmiths.com>
2023-02-28 22:01:34 -0500

    added essentiallEqual Function

--- Code/Utilities/utilFunc.cpp
+++ Code/Utilities/utilFunc.cpp
@@ -4,3 +4,8 @@
 double constrain(double minN, double maxN, double val){
     return std::min(maxN, std::max(minN, val));
 }
+
+bool essentiallyEqual(float a, float b, float epsilon)
+{
+    return std::abs(a - b) <= ( (std::abs(a) > std::abs(b) ? std::abs(b) : std::abs(a)) * epsilon);
+}
--- Code/Utilities/utilFunc.h
+++ Code/Utilities/utilFunc.h
@@ -1,1 +1,2 @@
 double constrain(double minN, double maxN, double val);
+bool essentiallyEqual(float a, float b, float epsilon);

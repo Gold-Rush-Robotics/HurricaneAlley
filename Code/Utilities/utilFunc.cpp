ea916d1438453017b6b0bb5115d650933eea3d42
philip <philip@randomsmiths.com>
2023-03-01 22:10:32 -0500

    Added Pose class

--- Code/Utilities/utilFunc.cpp
+++ Code/Utilities/utilFunc.cpp
@@ -1,5 +1,6 @@
 #include "utilFunc.h"
 #include <algorithm> 
+#include <math.h>
 
 double constrain(double minN, double maxN, double val){
     return std::min(maxN, std::max(minN, val));
@@ -9,3 +10,50 @@ bool essentiallyEqual(float a, float b, float epsilon)
 {
     return std::abs(a - b) <= ( (std::abs(a) > std::abs(b) ? std::abs(b) : std::abs(a)) * epsilon);
 }
+
+
+Pose::Pose(double x, double y, double theta){
+    Pose::x = x;
+    Pose::y = y;
+    Pose::theta = theta;
+}
+
+void Pose::update(double x, double y, double theta){
+    Pose::x = x;
+    Pose::y = y;
+    Pose::theta = theta;
+}
+
+int radtoDeg(double rad){
+    return rad * (180.0/M_PI);
+}
+
+double degToRad(int deg){
+    return deg * (M_PI/180.0);
+}
+
+
+void Pose::normalizeHeading(){
+
+    theta = degToRad(radtoDeg(theta) % 360);
+}
+
+
+double Pose::getDirection(Pose p){
+    double xLength = p.x - x;
+    double yLength = p.y - y;
+    return std::atan(yLength/xLength);
+}
+
+double Pose::getDistance(Pose p){
+    return std::sqrt(std::pow(p.x-x, 2) + std::pow(p.y-y, 2));
+}
+
+double Pose::getHeadingOffset(Pose p){
+    return p.theta - theta;
+}
+
+
+
+
+
--- Code/Utilities/utilFunc.h
+++ Code/Utilities/utilFunc.h
@@ -1,2 +1,18 @@
+#ifndef UtilityH
+#define UtilityH
+
 double constrain(double minN, double maxN, double val);
 bool essentiallyEqual(float a, float b, float epsilon);
+
+class Pose{
+    public:
+        Pose(double x, double y, double theta);
+        double getDistance(Pose p);
+        double getDirection(Pose p);
+        double getHeadingOffset(Pose p);
+        void normalizeHeading();
+        void update(double x, double y, double theta);
+        double x, y, theta;   
+};
+
+#endif

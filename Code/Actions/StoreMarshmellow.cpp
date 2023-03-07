f350ff45afa5df266a97caf93a7c4c640f06e525
philip <philip@randomsmiths.com>
2023-03-07 18:13:04 -0500

    add agitator and delay into the store marshmellow

--- Code/Actions/StoreMarshmellow.cpp
+++ Code/Actions/StoreMarshmellow.cpp
@@ -1,4 +1,6 @@
 #include "StoreMarshmellow.h"
+#include "DelayAction.h"
+#include "TurnOffAgitator.h"
 #include <string>
 
 std::string colors[] = {"EMPTY", "RED", "GREEN", "WHITE"};
@@ -6,8 +8,10 @@ std::string colors[] = {"EMPTY", "RED", "GREEN", "WHITE"};
 StoreMarshmellow::StoreMarshmellow(Revolver::MARSHMALLOWS color){
         StoreMarshmellow::color = color;
         name = "Load Marshmelllow (" + colors[color] + ")";
+        GoldRushAction::setNext(new DelayAction(1.3))->setNext(new TurnOffAgitator());
     }
 
 GoldRushAction* StoreMarshmellow::run(Robot* robot){
+        robot->revolver->turn_on_agitator();
         return robot->revolver->store_marshmallow(color) ? nextAction : this;
     }

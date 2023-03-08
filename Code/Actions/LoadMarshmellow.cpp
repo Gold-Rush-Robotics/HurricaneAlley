97336a22e989c1b8ece67c1244c1fd9a0052fb79
philip <philip@randomsmiths.com>
2023-03-07 19:24:02 -0500

    make load Marshmellow also pringle

--- Code/Actions/LoadMarshmellow.cpp
+++ Code/Actions/LoadMarshmellow.cpp
@@ -1,12 +1,16 @@
 #include "LoadMarshmellow.h"
 #include "../Robot/mechanisms/revolver.h"
+#include "Pringle.h"
+#include "DelayAction.h"
 
 LoadMarshmellow::LoadMarshmellow(Revolver::MARSHMALLOWS color){
         LoadMarshmellow::color = color;
         std::string colors[] = {"EMPTY", "RED", "GREEN", "WHITE"};
         name = "Load Marshmelllow (" + colors[color] + ")";
+        GoldRushAction::setNext(new Pringle(2));
     }
 
 GoldRushAction* LoadMarshmellow::run(Robot* robot){
     return robot->revolver->load_marshmallow(color) ? nextAction : this;
 }
+

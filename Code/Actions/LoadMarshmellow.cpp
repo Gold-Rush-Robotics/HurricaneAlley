bd5d7698cdba4be59a1a7b93324a16e62dc3b627
philip <philip@randomsmiths.com>
2023-03-07 19:00:58 -0500

    remove copy pasted code i didnt want

--- Code/Actions/LoadMarshmellow.cpp
+++ Code/Actions/LoadMarshmellow.cpp
@@ -7,7 +7,6 @@ std::string colors[] = {"EMPTY", "RED", "GREEN", "WHITE"};
 LoadMarshmellow::LoadMarshmellow(Revolver::MARSHMALLOWS color){
         LoadMarshmellow::color = color;
         name = "Load Marshmelllow (" + colors[color] + ")";
-        GoldRushAction::setNext(new DelayAction(1.3))->setNext(new TurnOffAgitator());
     }
 
 GoldRushAction* LoadMarshmellow::run(Robot* robot){

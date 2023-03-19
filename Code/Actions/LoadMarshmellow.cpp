5183cec1f1d1b5c04ef727007d96c6a218a4f04e
Sam Crane <samovarfuture@gmail.com>
2023-03-19 00:27:58 -0400

    Formatting

--- Code/Actions/LoadMarshmellow.cpp
+++ Code/Actions/LoadMarshmellow.cpp
@@ -4,11 +4,11 @@
 #include "DelayAction.h"
 
 LoadMarshmellow::LoadMarshmellow(Revolver::MARSHMALLOWS color){
-        LoadMarshmellow::color = color;
-        std::string colors[] = {"EMPTY", "RED", "GREEN", "WHITE"};
-        name = "Load Marshmelllow (" + colors[color] + ")";
-        GoldRushAction::setNext(new Pringle(2));
-    }
+    LoadMarshmellow::color = color;
+    std::string colors[] = {"EMPTY", "RED", "GREEN", "WHITE"};
+    name = "Load Marshmelllow (" + colors[color] + ")";
+    GoldRushAction::setNext(new Pringle(2));
+}
 
 GoldRushAction* LoadMarshmellow::run(Robot* robot){
     return robot->revolver->load_marshmallow(color) ? nextAction : this;

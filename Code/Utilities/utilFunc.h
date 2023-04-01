5695bf1828ad4f43d3c8aa6e3a93cd17ef8a17ae
samofuture <samovarfuture@gmail.com>
2023-03-31 20:33:23 -0400

    Added MARSHMALLOWS enum to util for color sensor

--- Code/Utilities/utilFunc.h
+++ Code/Utilities/utilFunc.h
@@ -4,4 +4,13 @@
 double constrain(double minN, double maxN, double val);
 bool essentiallyEqual(float a, float b, float epsilon);
 
+enum MARSHMALLOWS { 
+    EMPTY,
+    RED, 
+    GREEN, 
+    WHITE,
+    RED_CHIPS,
+    GREEN_CHIPS
+};
+
 #endif

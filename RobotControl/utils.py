1a049da797668933894a774a1639f42c187c16fe
c4glenn <philip@randomsmiths.com>
2023-04-04 23:42:24 -0400

    add a remap Util

--- RobotControl/utils.py
+++ RobotControl/utils.py
@@ -1,2 +1,7 @@
 def clampRange(start: float, stop: float, num: float) -> float:
     return max(start, min(stop, float))
+
+def reMap(OldValue:float, OldMin:float, OldMax:float, NewMax:float, NewMin:float):
+    OldRange = (OldMax - OldMin)  
+    NewRange = (NewMax - NewMin)  
+    return (((OldValue - OldMin) * NewRange) / OldRange) + NewMin

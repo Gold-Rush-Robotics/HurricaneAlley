d9ba79f8239106eaf69f82ec3cb4b744b6fd02fd
c4glenn <philip@randomsmiths.com>
2023-04-07 00:48:32 -0400

    create a delay action

---
+++ RobotControl/Actions/DelayAction.py
@@ -0,0 +1,24 @@
+from GoldRushAction import GoldRushAction
+from Robot.Robot import Robot
+from datetime import datetime
+
+
+class DelayAction(GoldRushAction):
+    endTime: float
+
+    def __init__(self, delayTime: float, description: str = ..., ) -> None:
+        """Construct Delay Action
+
+        Args:
+            delayTime (float): Seconds to delay
+            description (str, optional): description to print. Defaults to ....
+        """
+        super().__init__(description)
+        self.endTime = (datetime.utcnow() / 1000) + delayTime
+
+    def run(self, robot: Robot) -> GoldRushAction:
+        if((datetime.utcnow() / 1000) >= self.endTime):
+            return self.nextAction
+        else:
+            return self
+        

33c473069b7d87db1a653631043c94353d4f91b7
c4glenn <philip@randomsmiths.com>
2023-04-07 17:08:50 -0400

    create main action for marshmallow

---
+++ RobotControl/Actions/marshmallowActions.py
@@ -0,0 +1,11 @@
+from GoldRushAction import GoldRushAction
+from Robot.Robot import Robot
+
+class MarshAction(GoldRushAction):
+    def __init__(self, description: str = ...) -> None:
+        super().__init__(description)
+    
+
+    def run(self, robot: Robot) -> GoldRushAction:
+        
+        return super().run(robot)

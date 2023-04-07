d6d56ac0e27d41cf59ec049e6dd32b0b7ca77b96
c4glenn <philip@randomsmiths.com>
2023-04-07 17:53:27 -0400

    created the overall run archetecture

--- RobotControl/Actions/runActions.py
+++ RobotControl/Actions/runActions.py
@@ -1,14 +1,18 @@
 from GoldRushAction import GoldRushAction
 from Robot.Robot import Robot
+from StartupActions import StartupActions
+from foodActions import FoodAction
+from marshmallowActions import MarshAction
+from duckActions import DuckAction
+from placementActions import PlacementAction
 
 def getRun() -> GoldRushAction:
-    pass
-    #Startup.setNext()
-    #Food
-    #Marshmallow
-    #Duck
-    #Placement
-    #finalize
+    return StartupActions("Startup")\
+        .setNext(FoodAction("Food Action"))\
+        .setNext(MarshAction("Marshmellows"))\
+        .setNext(DuckAction("Duck sweep"))\
+        .setNext(PlacementAction("Placement Time"))\
+        .setNext(FireWorksAction("Fireworks"))
 
 def runEachLoop(robot: Robot):
     robot.drivetrain.updatePosition()

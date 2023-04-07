7f246d3bcabbf56850fac0afeabd625634eaa330
c4glenn <philip@randomsmiths.com>
2023-04-07 17:09:14 -0400

    made run actions

---
+++ RobotControl/Actions/runActions.py
@@ -0,0 +1,18 @@
+from GoldRushAction import GoldRushAction
+from Robot.Robot import Robot
+
+def getRun() -> GoldRushAction:
+    pass
+    #Startup.setNext()
+    #Food
+    #Marshmallow
+    #Duck
+    #Placement
+    #finalize
+
+def runEachLoop(robot: Robot):
+    robot.drivetrain.updatePosition()
+
+
+class FireWorksAction(GoldRushAction):
+    pass

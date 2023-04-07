7768586693c6337d1cef11a5fe717b7b2586281f
c4glenn <philip@randomsmiths.com>
2023-04-07 17:05:16 -0400

    create main file

---
+++ RobotControl/main.py
@@ -0,0 +1,12 @@
+from Actions.runActions import getRun, runEachLoop
+from Actions.GoldRushAction import GoldRushAction
+from Robot.Robot import Robot
+
+
+if __name__ == "__main__":
+    robot = Robot()
+
+    actionTree: GoldRushAction = getRun()
+    while(actionTree):
+        runEachLoop(robot)
+        actionTree = actionTree.run(robot)

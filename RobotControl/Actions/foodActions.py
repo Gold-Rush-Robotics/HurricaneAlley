2bc2beb385f82121bdac98f24b6ccf9e46ec003c
c4glenn <philip@randomsmiths.com>
2023-04-08 01:31:06 -0400

    finished food action

--- RobotControl/Actions/foodActions.py
+++ RobotControl/Actions/foodActions.py
@@ -11,10 +11,11 @@ class FoodAction(GoldRushAction):
     food2Red: bool = False
     loadStack: LoadStack
     placeStack: PlaceStack
+    flag: False
     def __init__(self, description: str = ...) -> None:
         super().__init__(description)
         self.loadStack = LoadStack("Load food chip")
-        self.placeStack = PlaceStack("Place ")
+        self.placeStack = PlaceStack("Place food Chip")
 
     def run(self, robot: Robot) -> GoldRushAction:
         match(self.state):
@@ -27,27 +28,23 @@ class FoodAction(GoldRushAction):
             case 2:
                 self.food2Red = robot.vision.detectRedFood()
             case 3:
-                load = self.loadStack.run(robot, (MarshmallowColors.REDFOOD) if self.food2Red else (MarshmallowColors.GREENFOOD))
+                load = self.loadStack.run(robot, (MarshmallowColors.REDFOOD) if self.food2Red else (MarshmallowColors.GREENFOOD), self.flag)
+                self.flag = not load
                 drive = robot.drivetrain.driveToPoint([FOOD2[0] + FOOD2[2], FOOD2[1], -PI/4])
                 if(load and drive):
+                    self.flag = False
                     state+=1
             case 4:
-                pass
-        """
-         init intake
-         drive to spot2
-         visionnnn
-         go to the top-Right of food2 (45 degrees)
-         Load the right food chip
-         Place it
-         go to the bottom right of food1 (135 degrees)
-         load the next food chip
-         place it
-
-         if(red):
-            
-         if(green):
-        
-
-        """
-        robot.drivetrain.position
+                if not self.placeStack.run(robot, False):
+                    state += 1
+            case 5:
+                load = self.loadStack.run(robot, (MarshmallowColors.GREENFOOD) if self.food2Red else (MarshmallowColors.REDFOOD), self.flag)
+                self.flag = not load
+                drive = robot.drivetrain.driveToPoint([FOOD1[0] + FOOD1[2], FOOD1[1] + FOOD1[3], -3*PI/4])
+                if(load and drive):
+                    self.flag = False
+                    state+=1
+            case 6:
+                if not self.placeStack.run(robot, False):
+                    return self.nextAction
+        return self

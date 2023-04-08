9a2b787fc5a8bb7167ccff31fc2dff0207d260f0
c4glenn <philip@randomsmiths.com>
2023-04-07 20:14:34 -0400

    worked on food action

--- RobotControl/Actions/foodActions.py
+++ RobotControl/Actions/foodActions.py
@@ -2,10 +2,12 @@ from GoldRushAction import GoldRushAction
 from Robot.Robot import Robot
 
 class FoodAction(GoldRushAction):
+    state: int = 0
     def __init__(self, description: str = ...) -> None:
         super().__init__(description)
     
 
     def run(self, robot: Robot) -> GoldRushAction:
-        
-        return super().run(robot)
+        match(self.state):
+            case 0:
+                robot.drivetrain.driveToPoint([])

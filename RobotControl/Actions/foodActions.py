f1cd04b4838ab315694e269899c673071c8bf83e
c4glenn <philip@randomsmiths.com>
2023-04-08 00:33:17 -0400

    food work

--- RobotControl/Actions/foodActions.py
+++ RobotControl/Actions/foodActions.py
@@ -1,13 +1,20 @@
 from GoldRushAction import GoldRushAction
+from marshmallowActions import LoadStack
 from Robot.Robot import Robot
+from Robot.Marshmallows import MarshmallowColors
+from placementActions import PlaceStack
 
 from constants import *
 
 class FoodAction(GoldRushAction):
     state: int = 0
+    food2Red: bool = False
+    loadStack: LoadStack
+    placeStack: PlaceStack
     def __init__(self, description: str = ...) -> None:
         super().__init__(description)
-    
+        self.loadStack = LoadStack("Load food chip")
+        self.placeStack = PlaceStack("Place ")
 
     def run(self, robot: Robot) -> GoldRushAction:
         match(self.state):
@@ -15,8 +22,17 @@ class FoodAction(GoldRushAction):
                 ## Intake init
                 state += 1 
             case 1:
-                robot.drivetrain.driveToPoint([FOOD2[0] + FOOD2[2] + 15, FOOD2[1] + (FOOD2[3]/2), PI/2])
-            
+                if robot.drivetrain.driveToPoint([FOOD2[0] + FOOD2[2] + 15, FOOD2[1] + (FOOD2[3]/2), PI/2], 5, 0.01):
+                    state += 1
+            case 2:
+                self.food2Red = robot.vision.detectRedFood()
+            case 3:
+                load = self.loadStack.run(robot, (MarshmallowColors.REDFOOD) if self.food2Red else (MarshmallowColors.GREENFOOD))
+                drive = robot.drivetrain.driveToPoint([FOOD2[0] + FOOD2[2], FOOD2[1], -PI/4])
+                if(load and drive):
+                    state+=1
+            case 4:
+                pass
         """
          init intake
          drive to spot2
@@ -31,6 +47,7 @@ class FoodAction(GoldRushAction):
          if(red):
             
          if(green):
-         
+        
 
         """
+        robot.drivetrain.position

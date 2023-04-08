ebd31865e636f9b3256bad8a41b76b56c069c3c2
c4glenn <philip@randomsmiths.com>
2023-04-07 22:02:01 -0400

    work on food action

--- RobotControl/Actions/foodActions.py
+++ RobotControl/Actions/foodActions.py
@@ -1,6 +1,8 @@
 from GoldRushAction import GoldRushAction
 from Robot.Robot import Robot
 
+from constants import *
+
 class FoodAction(GoldRushAction):
     state: int = 0
     def __init__(self, description: str = ...) -> None:
@@ -10,4 +12,18 @@ class FoodAction(GoldRushAction):
     def run(self, robot: Robot) -> GoldRushAction:
         match(self.state):
             case 0:
-                robot.drivetrain.driveToPoint([])
+                ## Intake init
+                state += 1 
+            case 1:
+                robot.drivetrain.driveToPoint([FOOD2[0] + FOOD2[2] + 15, FOOD2[1] + (FOOD2[3]/2), PI/2])
+            
+        """
+         init intake
+         drive to spot2
+         visionnnn
+         if(red):
+            
+         if(green):
+         
+
+        """

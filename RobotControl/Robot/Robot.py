14e8a969798ba897cee106698076c6fefe634453
c4glenn <philip@randomsmiths.com>
2023-04-07 21:16:55 -0400

    add vision to robot

--- RobotControl/Robot/Robot.py
+++ RobotControl/Robot/Robot.py
@@ -7,6 +7,7 @@ from Drivebase import Drivetrain
 from Duck import Duck
 from Marshmallows import Marshmallows
 from Intake import Intake
+from Vision.mainVision import VisionBuilder
 
 
 class Robot:
@@ -14,6 +15,7 @@ class Robot:
     duck: Duck
     marhmallow:  Marshmallows
     intake: Intake
+    vision: VisionBuilder
     def __init__(self) -> None:
         i2c = busio.I2C(SCL, SDA)
         pca = PCA9685(i2c)
@@ -21,4 +23,6 @@ class Robot:
         self.duck = Duck(pca)
         self.marhmallow = Marshmallows(pca, i2c)
         self.intake = Intake(pca)
+        self.vision = VisionBuilder()
+        
         

92e3a16d4bdebb03a71a17de7d82bbe61647e11e
c4glenn <philip@randomsmiths.com>
2023-04-09 16:50:44 -0400

    add backup

--- RobotControl/Actions/placementActions.py
+++ RobotControl/Actions/placementActions.py
@@ -23,21 +23,30 @@ class PlaceStack(GoldRushAction):
         self.raiseDelay = DelayAction(2, self.description + " Raising Pringle")
         self.closeDelay = DelayAction(1, self.description + " Closing Pringle")
         self.placeDuck = PlaceDuck(self.description + " Placing Duck")
+        self.backup = Robot.drivetrain.position
 
-
-    def run(self, robot: Robot, duck:bool) -> GoldRushAction:
+    def run(self, robot: Robot, duck:bool, backUPX: bool) -> GoldRushAction:
         match(self.state):
             case 0:
                 robot.marshmallow.place_pringle(True)
                 robot.marshmallow.set_pringle(PringleStates.TIGHT)
+                self.backup = Robot.drivetrain.position
+                self.backup[0 if backUPX else 1] -= 5
                 if not self.placeDelay.run(robot):
                     self.state = 1
             case 1:
                 robot.marshmallow.set_pringle(PringleStates.OPEN)
                 if not self.openDelay.run(robot):
-                    self.state = 2 if duck else 10
+                    self.state = 2 if duck else 3
             case 2:
-                if not 
+                if not self.placeDuck.run(robot):
+                    self.state = 3
+            case 3:
+                if not robot.drivetrain.driveToPoint(self.backup, 1, 0.1):
+                    self.state = 4
+            case 4:
+                robot.marshmallow.set_pringle(PringleStates.TIGHT)
+                robot.marshmallow.set_loader(False)
             case 10:
                 robot.marshmallow.inPringle = 0
         return self

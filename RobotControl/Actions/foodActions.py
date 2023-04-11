c0fea525c959719da713576906f0ad7246aad9a4
Sam Crane <samovarfuture@gmail.com>
2023-04-10 21:31:02 -0400

    Changed intake init to camera release

--- RobotControl/Actions/foodActions.py
+++ RobotControl/Actions/foodActions.py
@@ -20,11 +20,12 @@ class FoodAction(GoldRushAction):
     def run(self, robot: Robot) -> GoldRushAction:
         match(self.state):
             case 0:
-                ## Intake init
-                state += 1 
+                ## Camera Release
+                robot.intake.dm_separator_servo.run(DM_SEPARATOR_DUCK)
+                self.state += 1
             case 1:
                 if robot.drivetrain.driveToPoint([FOOD2[0] + FOOD2[2] + 15, FOOD2[1] + (FOOD2[3]/2), PI/2], 5, 0.01):
-                    state += 1
+                    self.state += 1
             case 2:
                 self.food2Red = robot.vision.detectRedFood()
             case 3:
@@ -33,17 +34,17 @@ class FoodAction(GoldRushAction):
                 drive = robot.drivetrain.driveToPoint([FOOD2[0] + FOOD2[2], FOOD2[1], -PI/4])
                 if(load and drive):
                     self.flag = False
-                    state+=1
+                    self.state+=1
             case 4:
                 if not self.placeStack.run(robot, False):
-                    state += 1
+                    self.state += 1
             case 5:
                 load = self.loadStack.run(robot, (MarshmallowColors.GREENFOOD) if self.food2Red else (MarshmallowColors.REDFOOD), self.flag)
                 self.flag = not load
                 drive = robot.drivetrain.driveToPoint([FOOD1[0] + FOOD1[2], FOOD1[1] + FOOD1[3], -3*PI/4])
                 if(load and drive):
                     self.flag = False
-                    state+=1
+                    self.state+=1
             case 6:
                 if not self.placeStack.run(robot, False):
                     return self.nextAction

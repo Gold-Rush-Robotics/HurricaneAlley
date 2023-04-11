52045065a312dafa0a11ababb4f4d5af9058c246
Sam Crane <samovarfuture@gmail.com>
2023-04-11 13:05:18 -0400

    Added logic to wait to start until the red led comes on

--- RobotControl/main.py
+++ RobotControl/main.py
@@ -2,12 +2,16 @@ from Actions.runActions import getRun, runEachLoop
 from Actions.GoldRushAction import GoldRushAction
 from Robot.Robot import Robot
 
+RED_THRESHOLD = 200
 
 if __name__ == "__main__":
     robot = Robot()
 
     actionTree: GoldRushAction = getRun()
     while(actionTree):
+        color = robot.start_sensor.color_rgb_bytes()
+        while color[0] < RED_THRESHOLD:
+            color = robot.start_sensor.color_rgb_bytes()
         runEachLoop(robot)
         actionTree = actionTree.run(robot)
         print(actionTree.description)

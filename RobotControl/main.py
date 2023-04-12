04ba69bce88eeb9b4b72d0a89ae5cb614f03a561
samofuture <54423694+samofuture@users.noreply.github.com>
2023-04-12 09:02:31 -0400

    Moved check light out of action tree loop

--- RobotControl/main.py
+++ RobotControl/main.py
@@ -7,11 +7,12 @@ RED_THRESHOLD = 200
 if __name__ == "__main__":
     robot = Robot()
 
+    color = robot.start_sensor.color_rgb_bytes()
+    while color[0] < RED_THRESHOLD:
+        color = robot.start_sensor.color_rgb_bytes()
+
     actionTree: GoldRushAction = getRun()
     while(actionTree):
-        color = robot.start_sensor.color_rgb_bytes()
-        while color[0] < RED_THRESHOLD:
-            color = robot.start_sensor.color_rgb_bytes()
         runEachLoop(robot)
         actionTree = actionTree.run(robot)
         print(actionTree.description)

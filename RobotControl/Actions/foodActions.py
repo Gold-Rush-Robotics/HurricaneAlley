9a8244d1e9e6d6fbacb648dd7dec50bd0bc272d5
samofuture <philip@randomsmiths.com>
2023-04-14 23:33:27 -0400

    fix camera release

--- RobotControl/Actions/foodActions.py
+++ RobotControl/Actions/foodActions.py
@@ -21,10 +21,12 @@ class FoodAction(GoldRushAction):
         match(self.state):
             case 0:
                 ## Camera Release
-                robot.intake.dm_separator_servo.run(DM_SEPARATOR_DUCK)
+                robot.intake.dm_separator_servo.run(DM_RELEASE)
+                input()
                 self.state += 1
             case 1:
                 if robot.drivetrain.driveToPoint([FOOD2[0] + FOOD2[2] + 15, FOOD2[1] + (FOOD2[3]/2), PI/2], 5, 0.01):
+                    input()
                     self.state += 1
             case 2:
                 self.food2Red = robot.vision.detectRedFood()
@@ -33,19 +35,23 @@ class FoodAction(GoldRushAction):
                 self.flag = not load
                 drive = robot.drivetrain.driveToPoint([FOOD2[0] + FOOD2[2], FOOD2[1], -PI/4])
                 if(load and drive):
+                    input()
                     self.flag = False
                     self.state+=1
             case 4:
                 if not self.placeStack.run(robot, False):
+                    input()
                     self.state += 1
             case 5:
                 load = self.loadStack.run(robot, (MarshmallowColors.GREENFOOD) if self.food2Red else (MarshmallowColors.REDFOOD), self.flag)
                 self.flag = not load
                 drive = robot.drivetrain.driveToPoint([FOOD1[0] + FOOD1[2], FOOD1[1] + FOOD1[3], -3*PI/4])
                 if(load and drive):
+                    input()
                     self.flag = False
                     self.state+=1
             case 6:
                 if not self.placeStack.run(robot, False):
+                    input()
                     return self.nextAction
         return self
